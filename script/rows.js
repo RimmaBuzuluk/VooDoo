368053ed4f75456d3f05d0377c99c369c097d1a9
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-09-28 20:40:54 +0300

    hidden dev

--- script/rows.js
+++ script/rows.js
@@ -1,11 +1,6 @@
-function calculateItemsPerRow() {
-    const screenWidth = window.innerWidth;
-    
-    if (screenWidth >= 1200) {
-      return 4; // Для широких экранов - 4 элемента в ряду
-    } else if (screenWidth >= 768) {
-      return 3; // Для планшетов - 3 элемента в ряду
-    } else {
-      return 1; // Для мобильных устройств - 1 элемент в ряду
-    }
-  }
+let cart=[];
+
+function addToCart(item){
+  cart.push(item);
+  // console.log('додано у кошик', item)
+}
--- script/script.js
+++ script/script.js
@@ -6,12 +6,13 @@ fetch(jsonFileUrl)
   .then(data => {
     const items = data.products;
 
-    console.log(items);
+    // console.log(items);
 
     // Разбиваем товары на строки, по 4 товара в каждой строке
     const rows = [];
     for (let i = 0; i < items.length; i += 4) {
       const rowItems = items.slice(i, i + 4).map(item => {
+        // console.log(item.id)
         return `
           <div class="item w-full md:w-1/4 lg:w-1/4 p-1 mb-16 " style="height: 402px;">
             <div class="rounded" style="height: 300px; border: 1px solid black;">
@@ -27,7 +28,8 @@ fetch(jsonFileUrl)
                 <div>${item.handle}</div>
               </div>
             </div>
-            <button class="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded w-full">
+            <button class="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded w-full" 
+            onClick="addToCart(${item.id})">
               ADD TO CART
             </button>
           </div>
@@ -44,3 +46,47 @@ fetch(jsonFileUrl)
 
 
 
+  let cart=[];
+
+  function addToCart(item){
+    cart.push(item);
+    // console.log('додано у кошик',item)
+    // console.log(cart)
+}
+
+
+// function showCartOverlay() {
+//     const cartOverlay = document.getElementById('cartOverlay');
+//     cartOverlay.classList.remove('hidden');
+//   }
+  
+//   function hideCartOverlay() {
+//     const cartOverlay = document.getElementById('cartOverlay');
+//     cartOverlay.classList.add('hidden');
+//   }
+
+
+  function addToCart(item) {
+    cart.push(item);
+    // console.log('додано у кошик', item);
+    // console.log(cart);
+    showCartOverlay();  // Показати елемент кошика при додаванні в кошик
+  }
+
+
+const cartButton = document.getElementById('cartButton');
+const cartOverlay = document.getElementById('cartOverlay');
+const closeButton = document.getElementById('closeButton');
+
+
+
+
+cartButton.addEventListener('click', () => {
+    cartOverlay.classList.remove('hidden'); // Видаляємо клас "hidden"
+    
+});
+
+closeButton.addEventListener('click', () => {
+    cartOverlay.classList.add('hidden'); // Додаємо клас "hidden"
+    
+});
